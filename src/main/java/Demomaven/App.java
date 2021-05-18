cd309417a7a31db9efe6d8caec2ad85a6ff15784
Safal Jaiswal <55182@hexaware.com>
2021-05-18 14:47:23 +0530

    first commit

---
+++ src/main/java/Demomaven/App.java
@@ -0,0 +1,13 @@
+package Demomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
